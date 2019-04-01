0e1b760ed9733a5066426eaeb5d9b8234b5efa00
吴志成 <1067064019@qq.com>
2019-04-01 17:33:45 +0800

    1：turbine集成 2：security集成

--- src/main/java/com/jhzx/bfp/trade/monitor/BfpTradeMonitorApplication.java
+++ src/main/java/com/jhzx/bfp/trade/monitor/BfpTradeMonitorApplication.java
@@ -2,14 +2,14 @@ package com.jhzx.bfp.trade.monitor;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableTurbine
 public class BfpTradeMonitorApplication {
 
 	public static void main(String[] args) {
